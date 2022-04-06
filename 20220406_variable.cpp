049f7d44408b7ba50ff7cf7d9be3d307174a5d7f
graciayohana <103086034+graciayohana@users.noreply.github.com>
2022-04-06 11:09:09 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream> 
+using namespace std;
+int main() {//begin of functon
+    int angka;
+    string nama; //deklarasi variabel
+    nama = "Gracia Yohana Siagian"; //masukkan value kedalam variabel
+    angka = 10.21;
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka;
+    
+}//end of function
