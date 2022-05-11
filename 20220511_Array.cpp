9bcc1f46267f60ce3236accae36f548561b957af
graciayohana <103086034+graciayohana@users.noreply.github.com>
2022-05-11 09:55:00 +0800

    Create 20220511_Array.cpp

---
+++ 20220511_Array.cpp
@@ -0,0 +1,17 @@
+//array
+//dimulai dari 0 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama[] = {
+        "David", 
+        "Kabila", 
+        "Bryan", 
+        "Sidney"
+    };
+    cout<<nama[2];
+    return 0;
+
+}
