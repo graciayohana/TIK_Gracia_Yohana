8d44ec8ae602e26f99655894b3b34ba4dc52d398
graciayohana <103086034+graciayohana@users.noreply.github.com>
2022-05-10 20:29:25 -0700

    Create 20220511_Array(matriks).cpp

---
+++ 20220511_Array(matriks).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka[5] [4] = {
+        {1, 2, 3, 4}, 
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+        {17, 18, 19, 20},
+    };
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<angka[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+    
+}
+
+
+bagian 2
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka[2] [2] = {
+        {},
+        {},
+    
+    };
+    
+    for 
+    cout<<int a[i][j] + b[i][j];
+    
+        
+    return 0;
+    
+}
