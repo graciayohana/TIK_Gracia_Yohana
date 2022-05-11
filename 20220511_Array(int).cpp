97146fb9c52aa9335dbfc2545da1421bf568b651
graciayohana <103086034+graciayohana@users.noreply.github.com>
2022-05-11 10:11:02 +0800

    Create 20220511_Array(int).cpp

---
+++ 20220511_Array(int).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int angka[10] = {
+        123, 
+        123, 
+        445, 
+        5456,
+        67,
+        6678,
+        867,
+        678,
+        6786,
+        678
+    };
+    
+    //cara panggil semua
+    for (int i = 0; i<10 ; i ++){
+        cout<<angka[i]<<endl;
+    }
+    return 0;
+  
+
+}
